5a5751fadbe6cde48d315f92c1bc79acd774126d
prathikshark <96731595+prathikshark@users.noreply.github.com>
2024-03-19 08:16:47 +0530

    ui

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -1,12 +1,39 @@
 class PaymentController < ApplicationController
  def index
+  @show_logout_cart=true;
 
  end
 
  def create
-   id=current_user.id
-   Userbook.where(user_id: id).destroy_all
-   redirect_to root_path, notice: 'Payment done!!!'
- end
+  
+ puts(params)
+cnum=params[:card_number]
+expiry=params[:expiry_date]
+cvv=params[:cvv]
 
+if  card_valid?(cnum) && expiry_valid?(expiry) && cvv_valid?(cvv)
+  id=current_user.id
+  Userbook.where(user_id: id).destroy_all
+  redirect_to root_path ,notice:"Payment done"
+else
+  redirect_to payment_path ,notice:"Invalid data"
+end
+
+
+
+private
+def card_valid?(cnum)
+  cnum.present? && cnum.length == 16 && cnum =~ /\A\d+\z/
+end
+
+def expiry_valid?(expire)
+  expiry.present? && expiry =~ /\A\d{2}\/\d{2}\z/
+end
+
+def cvv_valid?(cvv)
+  cvv.present? && cvv.length == 3 && cvv =~ /\A\d+\z/
+
+end
+
+end
 end
